3041b2123c24d9db753aca218b991cfa0195d132
Philips Jose <philipsjose20@gmail.com>
2025-12-01 18:27:52 +0530

    working

---
+++ Chess/MVVM/View/ChessBoardCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ChessBoardCollectionViewCell.swift
+//  Chess
+//
+//  Created by Philips Jose on 06/11/25.
+//
+
+import UIKit
+
+class ChessBoardCollectionViewCell: UICollectionViewCell {
+    
+    static let id = "ChessBoardCollectionViewCell"
+    
+    @IBOutlet weak var chessPieceImageView: UIImageView!
+    
+    var chessBoardCell: ChessBoardCell?
+    
+    func configure(cell: ChessBoardCell) {
+        self.chessBoardCell = cell
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        chessPieceImageView?.image = chessBoardCell?.piece?.asset
+        self.backgroundColor = chessBoardCell?.currentColor.uiColor
+    }
+}
