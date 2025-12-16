9ae15f9993aca898d072e3105aa27708790dab2a
Philips Jose <philipsjose2003@gmail.com>
2025-12-16 14:14:56 +0530

    Removed unwanted space

--- Chess/Helper/Helper.swift
+++ Chess/Helper/Helper.swift
@@ -12,23 +12,14 @@ class Helper {
             print("Error: File \(fileName) with extension \(fileExtension) not found in bundle.")
             return nil
         }
-        
         let fileURL = URL(fileURLWithPath: path)
         var storage: [String] = []
-        
         do {
-            // 1. Read the entire file content as a single string
             let fileContents = try String(contentsOf: fileURL, encoding: .utf8)
-            
-            // 2. Split the string into individual lines
             let lines = fileContents.components(separatedBy: .newlines)
-            
-            // 3. Process each line
             for line in lines {
-                // Ignore empty lines (often the last line)
                 let trimmedLine = line.trimmingCharacters(in: .whitespacesAndNewlines)
                 if trimmedLine.isEmpty { continue }
-                
                 storage.append(trimmedLine)
             }
             return storage
