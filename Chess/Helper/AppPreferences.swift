321163e0d9d3eff783371e09456456cd59a0b114
Philips Jose <philipsjose2003@gmail.com>
2026-01-28 18:04:32 +0530

    Added AppPreferences

---
+++ Chess/Helper/AppPreferences.swift
@@ -0,0 +1,55 @@
+//
+//  AppPreferences.swift
+//  Chess
+//
+//  Created by Philips Jose on 28/01/26.
+//
+
+import Foundation
+
+struct AppPreferenceKeys {
+    static let currentPlayerMode = "currentPlayerMode"
+}
+
+class AppPreferences {
+    
+    static let shared = AppPreferences()
+    
+    class func bool(forKey: String) -> Bool {
+        return UserDefaults.standard.bool(forKey: forKey)
+    }
+    
+    class func value(forKey: String) -> Any? {
+        return UserDefaults.standard.value(forKey: forKey)
+    }
+    
+    class func object(forKey: String) -> Any? {
+        return UserDefaults.standard.object(forKey: forKey)
+    }
+    
+    class func integer(forKey: String) -> Int {
+        return UserDefaults.standard.integer(forKey: forKey)
+    }
+    
+    class func string(forKey : String) -> String? {
+        return UserDefaults.standard.string(forKey: forKey)
+    }
+    
+    class func set(_ value: Any, forKey: String) {
+        UserDefaults.standard.set(value, forKey: forKey)
+        UserDefaults.standard.synchronize()
+    }
+}
+
+extension AppPreferences {
+    static var currentPlayerMode: PlayerMode {
+        get {
+            let id = AppPreferences.integer(forKey: AppPreferenceKeys.currentPlayerMode)
+            let mode = PlayerMode(rawValue: id) ?? .passAndPlay
+            return mode
+        }
+        set {
+            AppPreferences.set(newValue.rawValue, forKey: AppPreferenceKeys.currentPlayerMode)
+        }
+    }
+}
