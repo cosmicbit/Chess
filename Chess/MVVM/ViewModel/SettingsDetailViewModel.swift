8a72f3c1ea6865f587f76bd590f52ef31e9dc51a
Philips Jose <philipsjose2003@gmail.com>
2026-02-12 19:02:45 +0530

    wrote the initial setup

--- Chess/MVVM/ViewModel/SettingsDetailViewModel.swift
+++ Chess/MVVM/ViewModel/SettingsDetailViewModel.swift
@@ -4,8 +4,27 @@
 //
 //  Created by Philips Jose on 03/02/26.
 //
+import UIKit
 
 class SettingsDetailViewModel {
     var currentSetting: SettingsDestination = .appTheme
+    var currentSettingOptions: [String] = []
+    var currentSelectedOption: Int = 0
     
+    func initialSetup() {
+        
+        switch currentSetting {
+        case .boardTheme:
+            currentSettingOptions = BoardTheme.allCases.map({ $0.title })
+            currentSelectedOption = AppPreferences.shared.currentBoardTheme.rawValue
+        case .pieceStyle:
+            currentSettingOptions = PieceStyle.allCases.map({ $0.title })
+            currentSelectedOption = AppPreferences.shared.currentPieceStyle.rawValue
+        case .appTheme:
+            currentSettingOptions = AppTheme.allCases.map({ $0.title })
+            currentSelectedOption = AppPreferences.shared.currentAppTheme.rawValue
+        default:
+            break
+        }
+    }
 }
