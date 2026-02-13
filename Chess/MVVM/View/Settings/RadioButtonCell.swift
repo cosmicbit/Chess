a1f079ba51c37e06e9d236ae0a39afdf32d593bc
Philips Jose <philipsjose2003@gmail.com>
2026-02-13 12:34:48 +0530

    Added viewModel in radioButton cell

--- Chess/MVVM/View/Settings/RadioButtonCell.swift
+++ Chess/MVVM/View/Settings/RadioButtonCell.swift
@@ -14,6 +14,11 @@ class RadioButtonCell: UITableViewCell {
     @IBOutlet private weak var optionNameLabel: UILabel!
     @IBOutlet private weak var selectionStatusImageView: UIImageView!
     
+    public func configure(viewModel: RadioButtonViewModel) {
+        self.optionNameLabel.text = viewModel.optionName
+        //self.selectionStatusImageView.isHighlighted = viewModel.selected
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,8 +26,13 @@ class RadioButtonCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        self.selectionStatusImageView.image = selected ? UIImage(systemName: "circle.fill") : nil
         // Configure the view for the selected state
     }
 
+}
+
+struct RadioButtonViewModel {
+    let optionName: String
+    let selected: Bool
 }
