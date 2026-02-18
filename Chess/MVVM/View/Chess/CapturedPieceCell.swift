51f64e1b035964b068afe6107363d6972b28c790
Philips Jose <philipsjose2003@gmail.com>
2026-02-18 18:17:21 +0530

    Modified constraints of ui in chess vc

---
+++ Chess/MVVM/View/Chess/CapturedPieceCell.swift
@@ -0,0 +1,23 @@
+//
+//  CapturedPieceCell.swift
+//  Chess
+//
+//  Created by Philips Jose on 18/02/26.
+//
+
+import UIKit
+
+class CapturedPieceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
