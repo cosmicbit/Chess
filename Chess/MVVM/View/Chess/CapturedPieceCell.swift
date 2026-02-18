31ef6935d25418efa1ee1280769868e1328e9e3c
Philips Jose <philipsjose2003@gmail.com>
2026-02-18 18:19:32 +0530

    Added capture collection view cell

--- Chess/MVVM/View/Chess/CapturedPieceCell.swift
+++ Chess/MVVM/View/Chess/CapturedPieceCell.swift
@@ -7,17 +7,18 @@
 
 import UIKit
 
-class CapturedPieceCell: UITableViewCell {
+class CapturedPieceCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var chessPieceImageView: UIImageView!
+    //@IBOutlet private weak var
+    
+    public func configure(with image: UIImage? = nil) {
+        chessPieceImageView?.image = image
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
