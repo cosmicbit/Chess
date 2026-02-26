6fa34d90832d096d9dc31e4f089dce8063fb0d7e
Philips Jose <philipsjose2003@gmail.com>
2026-02-26 14:03:50 +0530

    Added a new model captured piece

---
+++ Chess/MVVM/Models/CapturedPiece.swift
@@ -0,0 +1,13 @@
+//
+//  CapturedPiece.swift
+//  Chess
+//
+//  Created by Philips Jose on 26/02/26.
+//
+
+
+struct CapturedPiece: Equatable {
+    let type: ChessPieceType
+    let color: ChessPieceColor
+    var count: Int
+}
